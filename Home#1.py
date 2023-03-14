5b10a986eebd0db750bc325b66034a8524e87a7f
Qwertymaster11 <127662574+Qwertymaster11@users.noreply.github.com>
2023-03-14 09:32:47 +0000

    commit#1

--- Home#1.py
+++ Home#1.py
@@ -1,12 +1,11 @@
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "Secret"
+openai.api_key = ""
 
-ai = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Tell me about apple.",
-  max_tokens=1024,
-  temperature=1
+ai = openai.Image.create(
+  prompt="A cute baby sea otter",
+  n=2,
+  size="1024x1024"
 )
 print(ai)
